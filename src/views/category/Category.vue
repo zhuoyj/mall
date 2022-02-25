b84c2095eaf97e7685050c64d8b770482806101e
哇偶 <9386757+zhuoyujian@user.noreply.gitee.com>
2022-02-25 22:11:41 +0800

    aaa

---
+++ src/views/category/Category.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>分类</h2>
+</template>
+
+<script>
+import BScroll from "better-scroll";
+export default {
+  name: "Category",
+  data() {
+    return {
+      scroll: null,
+    };
+  },
+  //组件创建完后调用
+  created() {
+    // this.scrool = new BScroll(".wrapper");
+  },
+  mounted() {
+    this.scroll = new BScroll(".wrapper", {
+      probeType: 3,
+    });
+
+    this.scroll.on("scroll", (position) => {});
+  },
+};
+</script>
+
+<style scoped>
+</style>
